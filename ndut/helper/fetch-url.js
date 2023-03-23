a8d6d1540bf5294b84930fb5e66f41103fcd12fa
Ardhi Lukianto <ardhi@lukianto.com>
2023-03-23 22:43:14 +0700

    Updates

--- ndut/helper/fetch-url.js
+++ ndut/helper/fetch-url.js
@@ -1,7 +1,17 @@
+const http = require('http')
+const https = require('https')
+
 module.exports = async function (url, options = {}) {
   const { aneka, getNdutConfig } = this.ndut.helper
   const cfg = getNdutConfig('ndut-extra')
   options.url = url
+  if (options.agent === false) {}
+  else {
+    const opt = options.agent || { keepAlive: true }
+    options.httpAgent = options.httpAgent || new http.Agent(opt)
+    options.httpsAgent = options.httpsAgent || new https.Agent(opt)
+  }
+
   return await aneka.fetchUrl(options, {
     cacheBuster: options.cacheBuster,
     cacheBusterKey: cfg.cacheBusterKey
